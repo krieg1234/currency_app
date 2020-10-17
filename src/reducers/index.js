b57a5ba6be3a5ad117f3d578c0890343b0019f27
krieg1234 <olegkrieg@gmail.com>
2020-10-17 17:26:30 +0400

    using localStorage for save favorite currencys

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,6 @@ import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
 import * as actions from '../actions';
 
-//import updateCurrencyRates from '../updateCurrencyRates';
-
 const sortCurrencyByFavorite = (currencys) => (curr1, curr2) => {
     if (currencys[curr1].isFavorite === currencys[curr2].isFavorite) return 0;
     if (currencys[curr1].isFavorite) return -1;
@@ -26,7 +24,7 @@ const currencys = handleActions({
         allCurrencys.forEach((c) => {
             mappedCurrencys.byCurrency[c] = {
                 rate: newCurrencys[c],
-                isFavorite: (state.byCurrency[c]) ? state.byCurrency[c].isFavorite : false
+                isFavorite: (localStorage.favorites)?localStorage.favorites.includes(c):false
             }
         });
        mappedCurrencys.allCurrencys.sort(sortCurrencyByFavorite(mappedCurrencys.byCurrency));
@@ -50,42 +48,45 @@ const currencys = handleActions({
 
 const baseCurrency = handleActions({
     [actions.switchBaseCurrency]: (state, action) => {
+        localStorage.setItem('baseCurrency',action.payload.baseCurrency);
         return action.payload.baseCurrency;
     }
-}, 'USD');
+}, (localStorage.baseCurrency)?localStorage.baseCurrency:'USD');
+
+const favorites=handleActions({
+    [actions.markFavorite]:(state, action)=>{
+        const {currName}=action.payload;
+        let newFavorites=[];
+        if (state.includes(currName)) {
+            newFavorites=state.filter((c)=>c!==currName);            
+        } else {
+            newFavorites=[...state, action.payload.currName];
+        };
+        localStorage.setItem('favorites', newFavorites);
+        return newFavorites;
+    }
+},(localStorage.favorites)?localStorage.favorites.split(','):[]);
 
 const defaultUI = {
     inputValue: '',
-    targetCurrency: 'USD'
+    targetCurrency: (localStorage.targetCurrency)?localStorage.targetCurrency:'USD'
 }
 const UI = (state = defaultUI, action) => {
     switch (action.type) {
-        case 'INPUT_EDIT': {
+        case 'INPUT_EDIT': {            
             return { ...state, inputValue: action.payload.text };
         }
         case 'TARGET_SELECT': {
+            localStorage.setItem('targetCurrency', action.payload.currName);
             return { ...state, targetCurrency: action.payload.currName };
         }
         default: return state;
     };
 };
 
-
-/*const ratesUpdateState = handleActions({
-    [actions.updateRatesRequest]() {
-        return 'requested';
-    },
-    [actions.updateRatesFailure]() {
-        return 'failed';
-    },
-    [actions.updateRatesSuccess]() {
-        return 'finished';
-    },
-}, 'none');*/
-
 export default combineReducers({
     currencys,
     baseCurrency,
     UI,
-    //ratesUpdateState
+    favorites
 });
