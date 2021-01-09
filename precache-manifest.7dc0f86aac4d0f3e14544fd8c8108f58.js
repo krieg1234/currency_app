0267b52df6b88abac6a76b5edf66691aa2b42154
krieg1234 <olegkrieg@gmail.com>
2021-01-09 19:06:31 +0400

    Updates

---
+++ precache-manifest.7dc0f86aac4d0f3e14544fd8c8108f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba10ced9f71ead668b24f7e01438feb",
+    "url": "/currency_app/index.html"
+  },
+  {
+    "revision": "0cec459ebf528144383b",
+    "url": "/currency_app/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "9059f6dd5d4bfb7661ec",
+    "url": "/currency_app/static/css/main.6b0df420.chunk.css"
+  },
+  {
+    "revision": "0cec459ebf528144383b",
+    "url": "/currency_app/static/js/2.cfc9996d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/currency_app/static/js/2.cfc9996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9059f6dd5d4bfb7661ec",
+    "url": "/currency_app/static/js/main.18029391.chunk.js"
+  },
+  {
+    "revision": "4c36d8d9ed61a1f303dc",
+    "url": "/currency_app/static/js/runtime-main.5e461fa2.js"
+  }
+]);
